ecd69b85d2cb494a98bea321bb304ec1276adf41
Aadithya-J <jlaadithya@gmail.com>
2025-06-07 18:14:27 +0530

    remove unused import of NextRequest from middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 const publicPaths = ["/login", "/register"];
